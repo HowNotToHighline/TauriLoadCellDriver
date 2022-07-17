75c4da701e5172ba6ed1e8ea8837bf9bce76f90a
Jetse Verschuren <jetse@verschurendegroot.nl>
2022-07-17 23:12:22 +0200

    Windows sucks
    I needed to manually specify the path of the labjack library to actually link it...

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,4 +1,7 @@
 fn main() {
     println!("cargo:rustc-link-lib=LabJackM");
+    if cfg!(windows) {
+        println!("cargo:rustc-link-search=C:\\Program Files (x86)\\LabJack\\Drivers\\64bit");
+    }
     tauri_build::build()
 }
